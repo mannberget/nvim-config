b965d72b53f88938ec2b17b1f65fc713eb9516f2
Noah Mannberg <noaman@foi.se>
2025-03-13 15:07:25 +0100

    add ghostty config etc

--- nvim/install.sh
+++ nvim/install.sh
@@ -16,6 +16,7 @@ repos_and_tags=(
     "stevearc/oil.nvim" "default"
     "folke/ts-comments.nvim" "default"
     "nvimtools/none-ls.nvim" "default"
+    "AlexvZyl/nordic.nvim" "default"
 )
 
 # Define the target directory for cloning
